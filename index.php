965944317ef7462fdc85a7c9baf68db7bff35615
HelenEmmett <helen.emmett@gmail.com>
2025-08-01 17:58:37 +1200

    Initial Commit
    Initial file set up and beginning of HTML code.

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <!-- necessary for responsive display -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>Calendar Project</title>
+    <meta name="description" content="My own calendar project">
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+    <header>
+
+    </header>
+
+</body>
+</html>
